9d11e01ae814fce0c66dd87f35083c0b10e9f0df
alannarisse <alannarisse@users.noreply.github.com>
2023-08-17 10:14:50 -0700

    added deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// vue.config.js file to be placed in the root of your repository
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vuejs-search-meals/'
+    : '/'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()]
 })
+
