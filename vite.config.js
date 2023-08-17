85fab9eb4191c43115d498efecbb1e28db029dc9
alannarisse <alannarisse@users.noreply.github.com>
2023-08-17 10:32:36 -0700

    removed dist from gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // vue.config.js file to be placed in the root of your repository
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vuejs-search-meals/'
-    : '/'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/vuejs-search-meals/'
+//     : '/'
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
